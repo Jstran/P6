668adae534707efa9fc6ec61981c7b5c470f5e49
Magnus Birkemose <mnorda@student.aau.dk>
2019-02-21 11:24:20 +0100

    Ny liste

--- el_ting_v1.R
+++ el_ting_v1.R
@@ -20,7 +20,7 @@ dat2016 <- dat2016[-60,]
 datesY <- seq(ymd("2013-01-01"), ymd("2018-12-31"), by="days")
 dates<-format(datesY, format="%d-%m")[1:365]
 
-# Samler hvert aar for DK2 i en data frame og liste.
+# Samler hvert aar for DK2 i en data frame.
 DK2f <- data.frame(numeric(365))
 row.names(DK2f) <- dates
 l <- 1
@@ -30,15 +30,7 @@ for (i in 2013:2018) {
   l <- l + 1
 }
 
-fq <- 7
-DK2 <- list(Y13 = ts(DK2f[,1], frequency = fq),
-            Y14 = ts(DK2f[,2], frequency = fq), 
-            Y15 = ts(DK2f[,3], frequency = fq), 
-            Y16 = ts(DK2f[,4], frequency = fq), 
-            Y17 = ts(DK2f[,5], frequency = fq), 
-            Y18 = ts(DK2f[,6], frequency = fq))
-
-# Data frama der indeholder alle år for DK2
+# Samler alle aar for DK2 i samme data frama.
 dfDK2 <- data.frame(numeric(2191))
 row.names(dfDK2) <- datesY
 dfDK2 <- c(DK2f[,1], 
@@ -48,9 +40,18 @@ dfDK2 <- c(DK2f[,1],
            DK2f[,5],
            DK2f[,6])
 
+# Samler hvert aar samt alle år sammen for DK1 i en liste
+fq <- 7
+DK2 <- list(Y13 = ts(DK2f[,1], frequency = fq),
+            Y14 = ts(DK2f[,2], frequency = fq), 
+            Y15 = ts(DK2f[,3], frequency = fq), 
+            Y16 = ts(DK2f[,4], frequency = fq), 
+            Y17 = ts(DK2f[,5], frequency = fq), 
+            Y18 = ts(DK2f[,6], frequency = fq),
+            YAll = ts(dfDK2,   frequency = fq))
 
 
-# Samler hvert aar for DK1 i en data frame og liste.
+# Samler hvert aar for DK1 i en data frame.
 DK1f <- data.frame(numeric(365))
 row.names(DK1f) <- dates
 l <- 1
@@ -59,14 +60,6 @@ for (i in 2013:2018) {
   names(DK1f)[l] <- i
   l <- l + 1
 }
-
-fq <- 7
-DK1 <- list(Y13 = ts(DK1f[,1], frequency = fq),
-            Y14 = ts(DK1f[,2], frequency = fq), 
-            Y15 = ts(DK1f[,3], frequency = fq), 
-            Y16 = ts(DK1f[,4], frequency = fq), 
-            Y17 = ts(DK1f[,5], frequency = fq), 
-            Y18 = ts(DK1f[,6], frequency = fq))
 
 # Data frama der indeholder alle år for DK1
 dfDK1 <- data.frame(numeric(2191))
@@ -78,10 +71,20 @@ dfDK1 <- c(DK1f[,1],
            DK1f[,5],
            DK1f[,6])
 
+# Samler hvert aar samt alle år sammen for DK1 i en liste
+fq <- 7
+DK1 <- list(Y13 = ts(DK1f[,1], frequency = fq),
+            Y14 = ts(DK1f[,2], frequency = fq), 
+            Y15 = ts(DK1f[,3], frequency = fq), 
+            Y16 = ts(DK1f[,4], frequency = fq), 
+            Y17 = ts(DK1f[,5], frequency = fq), 
+            Y18 = ts(DK1f[,6], frequency = fq),
+            YAll = ts(dfDK1,   frequency = fq))
+
 
 # Mean, sd og andre gode sager
-apply(DK2f, 2, mean)
-apply(DK1f, 2, mean)
+apply(DK2, 2, mean)
+apply(DK1, 2, mean)
 
 apply(DK2f, 2, sd)
 

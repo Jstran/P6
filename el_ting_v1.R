09c189504c8dc5b96391c5903d51d2d67598a355
csanda14 <csanda14@student.aau.dk>
2019-03-04 09:14:12 +0100

    njnj

--- el_ting_v1.R
+++ el_ting_v1.R
@@ -4,64 +4,157 @@ library(ggplot2)
 library(pracma)
 library(forecast)
 library(tseries)
+# DK1 : Jylland og Fyn, DK2: Sjælland , DK : Det hele 
 
+### Farver til brug i plots -------------------------------------------------------------
+colors <- c("royalblue4" ,
+            "firebrick4" ,
+            "darkorchid4",
+            "chartreuse4",
+            "black")
+
+### Indlæsning af data ------------------------------------------------------------------
 # Indlaeser csv filerne som "dat20xx".
 path <- file.path("./Data")
 years <- list.files(path,pattern =".csv",full.names = 1)
-years_names <- seq(2014, length.out = length(years))
+years_names <- seq(2013, length.out = length(years))
 
 for (l in 1:length(years_names)) {
   assign(paste("dat", years_names[l], sep = ""), read.csv2(years[l], skip = 2))
 }
 
 # Fjerner skudaar i 2016.
+dat2016LY <- dat2016
 dat2016 <- dat2016[-60,] 
 
+
+### Data frame og lister med data -------------------------------------------------------
 # Laver datoer.
-dates_temp <- seq(ymd("1000-01-01"), ymd("1000-12-31"), by="days")
-dates<-format(dates_temp, format="%d-%m")
+datesY <- seq(ymd("2013-01-01"), ymd("2018-12-31"), by="days")
+dates<-format(datesY, format="%d-%m")[1:365]
 
 # Samler hvert aar for DK2 i en data frame.
 DK2f <- data.frame(numeric(365))
 row.names(DK2f) <- dates
 l <- 1
-for (i in 2014:2018) {
+for (i in 2013:2018) {
   DK2f[,l] <- get(paste("dat", i, sep=""))[,9]
   names(DK2f)[l] <- i
   l <- l + 1
 }
 
-fq <- 7
-DK2 <- data.frame(Y14 = ts(DK2f[,1], frequency = fq), Y15 = ts(DK2f[,2], frequency = fq), Y16 = ts(DK2f[,3], frequency = fq), Y17 = ts(DK2f[,4], frequency = fq), Y18 = ts(DK2f[,5], frequency = fq))
+# Samler alle aar for DK2 i samme data frama.
+dfDK2 <- data.frame(DK2 = numeric(2191), sat = numeric(2191), sun = numeric(2191))
+row.names(dfDK2) <- datesY
+dfDK2[,1] <- c(DK2f[,1], 
+               DK2f[,2],
+               DK2f[,3],
+               dat2016LY[,9],
+               DK2f[,5],
+               DK2f[,6])
+
+# Lørdag dummy, 01-01-13 er en tirsdag
+sat <- numeric(nrow(dfDK2))
+sat[seq(5, 2191, by = 7)] <- 1
+dfDK2[,2] <- sat
+
+# Søndag dummy, 01-01-13 er en tirsdag
+sun <- numeric(nrow(dfDK2))
+sun[seq(6, 2191, by = 7)] <- 1
+dfDK2[,3] <- sun
+
+# Samler hvert aar samt alle år sammen for DK1 i en liste
+fq <- 1 
+DK2 <- list(Y13  = ts(DK2f[,1],  frequency = fq),
+            Y14  = ts(DK2f[,2],  frequency = fq), 
+            Y15  = ts(DK2f[,3],  frequency = fq), 
+            Y16  = ts(DK2f[,4],  frequency = fq), 
+            Y17  = ts(DK2f[,5],  frequency = fq), 
+            Y18  = ts(DK2f[,6],  frequency = fq),
+            YAll = ts(dfDK2[,1], frequency = fq),
+            sat  = sat,
+            sun  = sun)
 
 
-# Mean, sd og andre gode sager
-apply(DK2f, 2, mean)
+# Samler hvert aar for DK1 i en data frame.
+DK1f <- data.frame(DK1 = numeric(365))
+row.names(DK1f) <- dates
+l <- 1
+for (i in 2013:2018) {
+  DK1f[,l] <- get(paste("dat", i, sep=""))[,8]
+  names(DK1f)[l] <- i
+  l <- l + 1
+}
 
-apply(DK2f, 2, sd)
+# Data frama der indeholder alle år for DK1
+dfDK1 <- data.frame(DK1 = numeric(2191), sat = numeric(2191), sun = numeric(2191))
+row.names(dfDK1) <- datesY
+dfDK1[,1] <- c(DK1f[,1], 
+               DK1f[,2],
+               DK1f[,3],
+               dat2016LY[,8],
+               DK1f[,5],
+               DK1f[,6])
+
+# Lørdag dummy, 01-01-13 er en tirsdag
+sat <- numeric(nrow(dfDK1))
+sat[seq(5, 2191, by = 7)] <- 1
+dfDK1[,2] <- sat
+
+# Søndag dummy, 01-01-13 er en tirsdag
+sun <- numeric(nrow(dfDK1))
+sun[seq(6, 2191, by = 7)] <- 1
+dfDK1[,3] <- sun
+
+# Samler hvert aar samt alle år sammen for DK1 i en liste
+fq <- 1
+DK1 <- list(Y13  = ts(DK1f[,1],  frequency = fq),
+            Y14  = ts(DK1f[,2],  frequency = fq), 
+            Y15  = ts(DK1f[,3],  frequency = fq), 
+            Y16  = ts(DK1f[,4],  frequency = fq), 
+            Y17  = ts(DK1f[,5],  frequency = fq), 
+            Y18  = ts(DK1f[,6],  frequency = fq),
+            YAll = ts(dfDK1[,1], frequency = fq),
+            sat  = sat,
+            sun  = sun)
+
+
+### Mean, sd, acf, pacf, decompose, plot med lag ----------------------------------------
+
+# Mean
+mean(DK1$YAll)
+
+# var
+var(DK1$YAll)
 
 # Nogle plots.
-plot(DK2$Y14, type = "l")
-lines(DK2$Y15, col = "red")
+par(mfrow = c(2,1))
+acf(DK1$YAll)
 
-plot(decompose(DK2$Y14))
+pacf(DK1$YAll)
 
-diffDK2Y14 <- diff(DK2$Y14, differences = 1)
+#plot(decompose(DK1$YAll))
 
-detrend = detrend(dat2016[,9], 'linear'); plot(ts(detrend, frequency = 365))
-trend2016 = ma(DK2$Y16, order = 7); plot(DK2$Y16 - trend2016)
 
-data_ts = ts(c(dat2014[,8],dat2015[,8],dat2016[,8],dat2017[,8],dat2018[,8],dat2019[,8]))
-timeseries = ts(data_ts, frequency = 365, start= c(2014,1))
+# Scatter plot med lag.
+par(mfrow = c(2,1))
+plot(DK1$Y14, lag(DK1$Y14,2),main = "2014", xlab = "x_t", ylab = "x_{t-1}")
+plot(DK1$Y15, lag(DK1$Y15,2),main = "2015", xlab = "x_t", ylab = "x_{t-1}")
+par(mfrow = c(1,1))
 
-plot(data_ts)
-plot(ts(dat2018[,8]))
-par(new = T)
-plot(ts(dat2018[,10]),col="red")
 
-plot(decompose(timeseries))
-acf(timeseries, lag = 50)
-pacf(timeseries, lag = 50)
+### ggplot af rå data -------------------------------------------------------------------
+pDK1YAll <-  ggplot(data.frame(X1 = datesY, 
+                               X2 = DK1$YAll), 
+                    aes(x = X1 , y = X2)) +
+  geom_line(aes(col = "DK1")) +
+  labs(x = "Tid", y = "Spotpris i DKK", title = "DK1 2013-2018: Spotpriser", 
+       color = "") +
+  scale_color_manual(values = colors[1]) +
+  ylim(-100, 500)
 
-tsclean(timeseries)
+pDK1YAll
+
+### outlier
+
 

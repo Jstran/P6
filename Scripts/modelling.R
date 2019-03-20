bed4fe87455064d3b4a5e288773d0c3628139e20
Jstran <jstran16@student.aau.dk>
2019-03-20 14:40:08 +0100

    Modellering er ikke tom

--- Scripts/modelling.R
+++ Scripts/modelling.R
@@ -10,9 +10,27 @@ library(ggplot2)
 library(forecast)
 library(astsa)
 
+### ¤¤ Regression ¤¤ ### ----------------------------------------------------------------
+
+# Regression på s model med kvadratisk led (t^2)
+t     <- time(DK1$A)
+s.lm <- lm(DK1$A ~ t + I(t^2) + 
+             sin((2*pi/365.25)*t) + cos((2*pi/365.25)*t) + 
+             sin((4*pi/365.25)*t) + cos((4*pi/365.25)*t) +
+             sin((8*pi/365.25)*t) + cos((8*pi/365.25)*t) +
+             DK1$sat + DK1$sun + DK1$hol)
+
+s.pred  <- predict(s.lm)
+
+DK1[[length(DK1)+1]] <- c(s.pred)
+names(DK1)[[length(DK1)]] <- "s.pred"
+
+DK1[[length(DK1)+1]] <- c(DK1$A - s.pred)
+names(DK1)[[length(DK1)]] <- "D"
+
 ### ¤¤ Gemmer workspace ¤¤ ### ----------------------------------------------------------
 
-save(n.obs, 
+save(t , s.lm, s.pred, DK1, 
      file = "./Workspaces/modelling.Rdata")
 
 ### ¤¤ Det vilde vesten ¤¤ ### ----------------------------------------------------------
--- Scripts/preliminary.R
+++ Scripts/preliminary.R
@@ -154,27 +154,9 @@ rm("dat2013","dat2014","dat2015","dat2016","dat2017","dat2018","dat2019","fq","i
    "wind2015", "wind2016", "wind2017", "wind2018", "years.names.win" , "years.win", 
    "path.win")
 
-### ¤¤ Regression ¤¤ ### ----------------------------------------------------------------
-
-# Regression på Escribano model med kvadratisk led (t^2)
-t     <- time(DK1$A)
-s.lm <- lm(DK1$A ~ t + I(t^2) + 
-              sin((2*pi/365.25)*t) + cos((2*pi/365.25)*t) + 
-              sin((4*pi/365.25)*t) + cos((4*pi/365.25)*t) +
-              sin((8*pi/365.25)*t) + cos((8*pi/365.25)*t) +
-              DK1$sat + DK1$sun + DK1$hol)
-
-s.pred  <- predict(s.lm)
-
-DK1[[length(DK1)+1]] <- c(s.pred)
-names(DK1)[[length(DK1)]] <- "s.pred"
-
-DK1[[length(DK1)+1]] <- c(DK1$A - s.pred)
-names(DK1)[[length(DK1)]] <- "D"
-
 ### ¤¤ Gemmer workspace ¤¤ ### ----------------------------------------------------------
 
-save(DK1, p6, colors, dates, n.obs, s.lm, s.pred, t, ci, scoef, sz, 
+save(DK1, p6, colors, dates, n.obs, ci, scoef, sz, 
      file = "./Workspaces/preliminary.Rdata")
 
 ### ¤¤ Det vilde vesten ¤¤ ### ----------------------------------------------------------

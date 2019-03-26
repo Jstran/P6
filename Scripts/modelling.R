8a3b8e4fc517c85f5dec8b9dac9771a28e43b98d
Jstran <jstran16@student.aau.dk>
2019-03-26 15:45:10 +0100

    Nu er han

--- Scripts/modelling.R
+++ Scripts/modelling.R
@@ -84,3 +84,9 @@ save(t , s.lm, s.pred, DK1,
      file = "./Workspaces/modelling.Rdata")
 
 ### ¤¤ Det vilde vesten ¤¤ ### ----------------------------------------------------------
+
+vec = rep(mean(DK1$D[1:2191]),2190)
+meanrev = lm(diff(DK1$D) ~ vec-DK1$D[1:2190])
+
+
+meanrev2 = lm(diff(DK1$D)~DK1$D[1:2190]-1);summary(meanrev2)

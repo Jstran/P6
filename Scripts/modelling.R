c3baabce2c9e19833904f7227b32172dc9366ad9
Magnus Birkemose <mnorda@student.aau.dk>
2019-03-29 13:57:35 +0100

    First try

--- Scripts/modelling.R
+++ Scripts/modelling.R
@@ -87,3 +87,32 @@ save(t , s.lm, s.pred, DK1,
 
 # Ronald alpha_0 mean reverision (på al data)
 meanrev2 = lm(diff(DK1$D)~DK1$D[1:2190]-1);summary(meanrev2)
+
+mse <- numeric(1152)
+for (i in 0:24) {
+  glob.lm <- lm(DK1$A[1:(1461 + 24*i)] ~ t[1:(1461 + 24*i)] + I(t[1:(1461 + 24*i)]^2) + I(t[1:(1461 + 24*i)]^3) + I(t[1:(1461 + 24*i)]^4) +
+                      sin((2*pi/365.25)*t[1:(1461 + 24*i)]) + cos((2*pi/365.25)*t[1:(1461 + 24*i)]) + 
+                      sin((4*pi/365.25)*t[1:(1461 + 24*i)]) + cos((4*pi/365.25)*t[1:(1461 + 24*i)]) +
+                      sin((8*pi/365.25)*t[1:(1461 + 24*i)]) + cos((8*pi/365.25)*t[1:(1461 + 24*i)]) +
+                      sin((24*pi/365.25)*t[1:(1461 + 24*i)]) + cos((24*pi/365.25)*t[1:(1461 + 24*i)]) +
+                      DK1$sat[1:(1461 + 24*i)] + DK1$sun[1:(1461 + 24*i)] + DK1$hol[1:(1461 + 24*i)] , na.action = "na.fail")
+
+  lm.combinations <- lapply(dredge(glob.lm , 
+                          evaluate = FALSE,
+                          subset = dc(sin((2*pi/365.25)*t[1:1461 + 24*i])  , 
+                                      cos((2*pi/365.25)*t[1:1461 + 24*i])  ,
+                                      sin((4*pi/365.25)*t[1:1461 + 24*i])  , 
+                                      cos((4*pi/365.25)*t[1:1461 + 24*i])  ,
+                                      sin((8*pi/365.25)*t[1:1461 + 24*i])  , 
+                                      cos((8*pi/365.25)*t[1:1461 + 24*i])  ,
+                                      sin((24*pi/365.25)*t[1:1461 + 24*i]) , 
+                                      cos((24*pi/365.25)*t[1:1461 + 24*i]) )), eval)
+  print(i)
+}
+  for (l in 1:5) {
+    mse[l] <- mse[l] + (DK1$A[1:(1461 + 24*i)] - predict(lm.combinations[[l]]))/length(1:(1461 + 24*i))
+  }
+  
+
+  
+
